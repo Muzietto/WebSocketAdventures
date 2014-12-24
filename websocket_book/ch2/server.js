48cbf133b77a6f9ce7a64b214ce4434452668ead
Muzietto <marco_faustinelli@yahoo.com>
2014-12-24 17:56:41 +0100

    two chapters of the book

---
+++ websocket_book/ch2/server.js
@@ -0,0 +1,68 @@
+
+var WebSocketServer = require('ws').Server,
+server = new WebSocketServer({
+    port : 8181
+  });
+var stockUpdater;
+
+var stocks = {
+  "AAPL" : 95.0,
+  "MSFT" : 50.0,
+  "AMZN" : 300.0,
+  "GOOG" : 550.0,
+  "YHOO" : 35.0
+}
+
+randomStockUpdater();
+
+server.on('connection', function (socket) {
+  var clientStockUpdater;
+  var clientStocks = [];
+
+  console.log('client is connected');
+
+  clientStockUpdater = setInterval(function () {
+      sendStockUpdates(socket);
+    }, 1000);
+
+  socket.on('message', function (message) {
+    var stock_request = JSON.parse(message);
+    clientStocks = stock_request['stocks'];
+    sendStockUpdates(socket);
+  });
+
+  socket.on('close', function () {
+    if (typeof clientStockUpdater !== 'undefined') {
+      clearInterval(clientStockUpdater);
+    }
+  });
+
+  function sendStockUpdates (socket) {
+    if (socket.readyState == 1) {
+      var stocksObj = {};
+      for (var i = 0; i < clientStocks.length; i++) {
+        symbol = clientStocks[i];
+        stocksObj[symbol] = stocks[symbol];
+      }
+      socket.send(JSON.stringify(stocksObj));
+    }
+  }
+});
+
+function randomStockUpdater() {
+  for (var symbol in stocks) {
+    if (stocks.hasOwnProperty(symbol)) {
+      var randomizedChange = randomInterval(-150, 150);
+      var floatChange = randomizedChange / 100;
+      stocks[symbol] += floatChange;
+    }
+  }
+  var randomMSTime = randomInterval(500, 2500);
+  stockUpdater = setTimeout(function () {
+      randomStockUpdater();
+    }, randomMSTime)
+}
+
+function randomInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
